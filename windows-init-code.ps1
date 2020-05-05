59986399a3badb2ab61542ae5fa103fb2d5fe693
Adam Long <adam@adam-long.net>
2020-05-05 17:14:12 -0400

    Fixed syntax error

--- windows-init-code.ps1
+++ windows-init-code.ps1
@@ -3,7 +3,7 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Updates
-Install-Module -Name PSWindowsUpdate
+Install-Module -Name PSWindowsUpdate -Force
 Get-WindowsUpdate -Install
 
 # Eliminate Bloatware
