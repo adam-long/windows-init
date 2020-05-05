a944428d43a329392d3aa862213095e897b3da5d
Adam Long <adam@adam-long.net>
2020-05-05 16:43:39 -0400

    Got rid of arguments powershell said were invalid

--- windows-init-code.ps1
+++ windows-init-code.ps1
@@ -2,17 +2,17 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Updates
-Install-Module -Name PSWindowsUpdate –Force
+Install-Module -Name PSWindowsUpdate
 Get-WindowsUpdate –Install
 
 # Eliminate Bloatware
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Sycnex/Windows10Debloater/master/Windows10SysPrepDebloater.ps1') + ' -Debloat')
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Sycnex/Windows10Debloater/master/Windows10SysPrepDebloater.ps1'))
 
 # Install basics
 choco install -y 7zip
 choco install -y notepadplusplus
 choco install -y wsl
-choco install -y googlechrome
+choco install -y googlechrome --ignore-checksum
 choco pin -y googlechrome
 choco install -y firefox
 choco pin -y firefox
@@ -27,11 +27,12 @@ choco install -y wsl-ubuntu-1804
 choco pin -y wsl-ubuntu-1804
 
 # Code
-choco install -y ojdkbuild11
 choco install -y git
 choco install -y gitextensions
 choco install -y openjdk11
 choco install -y vscode
 choco install -y apache-netbeans.portable
 choco install -y github-desktop
+choco pin -y github-desktop
 choco install -y typora
+choco pin -y typora
